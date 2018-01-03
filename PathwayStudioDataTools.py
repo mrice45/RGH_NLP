b57d6043b37e783354eae51460d8b1aa0105a75d
mrice88 <mark.rice.jr@gmail.com>
2018-01-02 16:40:37 -0800

    Updated wizard.py to be more pythonic. Added more words of interest to pos and neg, added docstrings to PSDT, as well as a write_csv function

--- PathwayStudioDataTools.py
+++ PathwayStudioDataTools.py
@@ -8,38 +8,75 @@ import pandas as pd
 
 
 def ps_read_csv(filename):
-    raw_data = pd.read_csv(filename, encoding='utf-8')
+    """
+    Read CSV, and make sure encoding is UTF-16
+    :param filename: File to be read
+    :return: Raw pandas DataFrame
+    """
+    raw_data = pd.read_csv(filename, encoding='UTF-16')
     return raw_data
 
-def ps_clean_csv(raw_data):
+# def ps_clean_csv(raw_data):
 
 
-def ps_clean_ref(ref):
+def ps_clean_ref(network):
+    """
+    Take in network, clean references column of PS tags
+    :param network: network to have references cleaned from
+    :return: Network, with new Clean_References column.
+    """
     clean = []
-    for i in range(len(ref)):
-        clean.append((ps_clean_sentence(ref['Ref'][i])))
+    for i in range(len(network)):
+        clean.append((ps_reformat_sentence(network['msrc'][i])))
 
-    ref['Clean_Rf'] = clean
-    return ref
+    network['Clean_References'] = clean
+    return network
 
 
-def ps_clean_sentence(s):
+def ps_reformat_sentence(s):
+    """
+
+    :param s:
+    :return:
+    """
     s = ps_clear_end(s)
     s = ps_replace_syntax(s)
-    return (s)
+    return s
 
 
 def ps_clear_end(s):
+    """
+
+    :param s:
+    :return:
+    """
     clean = re.sub('(?<=\.)[\s\S]+', '', s)
     return clean
 
 
 def ps_replace_syntax(s):
-    clean = re.sub('(ID{.*?})', ps_find_subject, s)
+    '''
+    Takes in sentence s and removes PS internal formatting
+    :param s: Sentence to be formatted
+    :return: cleaned sentence.
+    '''
+    clean = re.sub('(ID{.*?})', __ps_find_subject, s)
     return clean
 
 
-def ps_find_subject(s):
+def __ps_find_subject(s):
+    '''PS Internal formatting of subject to just plain-text'''
     plhldr = s.group(0)
     matchobj = re.search('(?<=\=).*?(?=})', plhldr)
     return matchobj.group(0)
+
+
+def ps_write_csv(network, csv_name='network_please_rename.csv'):
+    '''
+    Write network, formatted to CSV
+
+    Args:
+        network = network to be written to csv.
+        sav_name = Name for new file, defaulted 'network_please_rename.csv'
+    '''
+    network.to_csv(csv_name, encoding="UTF-16")
--- wizard.py
+++ wizard.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 """
 This module is based off of the below article:
@@ -10,40 +9,33 @@ I took what Javiar was doing and ran with it. A 1000 thanks mate :-)
 import nltk
 import yaml
 
-class Splitter(object):
 
-    def __init__(self):
-        self.nltk_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
-        self.nltk_tokenizer = nltk.tokenize.TreebankWordTokenizer()
+def split(text):
+    """
+    input format: a paragraph of text
+    output format: a list of lists of words.
+        e.g.: [['this', 'is', 'a', 'sentence'], ['this', 'is', 'another', 'one']]
+    """
 
-    def split(self, text):
-        """
-        input format: a paragraph of text
-        output format: a list of lists of words.
-            e.g.: [['this', 'is', 'a', 'sentence'], ['this', 'is', 'another', 'one']]
-        """
-        return [self.nltk_tokenizer.tokenize(sent) for sent in self.nltk_splitter.tokenize(text)]
+    nltk_splitter = nltk.data.load('tokenizers/punkt/english.pickle')
+    nltk_tokenizer = nltk.tokenize.TreebankWordTokenizer()
+    return [nltk_tokenizer.tokenize(sent) for sent in nltk_splitter.tokenize(text)]
 
 
+def pos_tag(self, sentences):
+    """
+    input format: list of lists of words
+        e.g.: [['this', 'is', 'a', 'sentence'], ['this', 'is', 'another', 'one']]
+    output format: list of lists of tagged tokens. Each tagged tokens has a
+    form, a lemma, and a list of tags
+        e.g: [[('this', 'this', ['DT']), ('is', 'be', ['VB']), ('a', 'a', ['DT']), ('sentence', 'sentence', ['NN'])],
+                [('this', 'this', ['DT']), ('is', 'be', ['VB']), ('another', 'another', ['DT']), ('one', 'one', ['CARD'])]]
+    """
 
-class POSTagger(object):
-    def __init__(self):
-        pass
-
-    def pos_tag(self, sentences):
-        """
-        input format: list of lists of words
-            e.g.: [['this', 'is', 'a', 'sentence'], ['this', 'is', 'another', 'one']]
-        output format: list of lists of tagged tokens. Each tagged tokens has a
-        form, a lemma, and a list of tags
-            e.g: [[('this', 'this', ['DT']), ('is', 'be', ['VB']), ('a', 'a', ['DT']), ('sentence', 'sentence', ['NN'])],
-                    [('this', 'this', ['DT']), ('is', 'be', ['VB']), ('another', 'another', ['DT']), ('one', 'one', ['CARD'])]]
-        """
-
-        pos = [nltk.pos_tag(sentence) for sentence in sentences]
-        # adapt format
-        pos = [[(word, word, [postag]) for (word, postag) in sentence] for sentence in pos]
-        return pos
+    pos = [nltk.pos_tag(sentence) for sentence in sentences]
+    # adapt format
+    pos = [[(word, word, [postag]) for (word, postag) in sentence] for sentence in pos]
+    return pos
 
 
 class DictionaryTagger(object):
@@ -71,29 +63,34 @@ class DictionaryTagger(object):
             - longest matches have higher priority
             - search is made from left to right
         """
+
         tag_sentence = []
         N = len(sentence)
+
         if self.max_key_size == 0:
             self.max_key_size = N
         i = 0
+
         while (i < N):
-            j = min(i + self.max_key_size, N) #avoid overflow
+            j = min(i + self.max_key_size, N)  # avoid overflow
             tagged = False
+
             while (j > i):
                 expression_form = ' '.join([word[0] for word in sentence[i:j]]).lower()
                 expression_lemma = ' '.join([word[1] for word in sentence[i:j]]).lower()
+
                 if tag_with_lemmas:
                     literal = expression_lemma
                 else:
                     literal = expression_form
                 if literal in self.dictionary:
-                    #self.logger.debug("found: %s" % literal)
+                    # self.logger.debug("found: %s" % literal)
                     is_single_token = j - i == 1
                     original_position = i
                     i = j
                     taggings = [tag for tag in self.dictionary[literal]]
                     tagged_expression = (expression_form, expression_lemma, taggings)
-                    if is_single_token: #if the tagged literal is a single token, conserve its previous taggings:
+                    if is_single_token:  # if the tagged literal is a single token, conserve its previous taggings:
                         original_token_tagging = sentence[original_position][2]
                         tagged_expression[2].extend(original_token_tagging)
                     tag_sentence.append(tagged_expression)
@@ -121,9 +118,9 @@ def sentence_score(sentence_tokens, previous_token, acum_score):
         token_score = sum([__value_of(tag) for tag in tags])
         if previous_token is not None:
             previous_tags = previous_token[2]
-            #if 'inc' in previous_tags:
+            # if 'inc' in previous_tags:
             #    token_score *= 2.0
-            #elif 'dec' in previous_tags:
+            # elif 'dec' in previous_tags:
             #    token_score /= 2.0
             if 'inv' in previous_tags:
                 token_score *= -1.0
@@ -138,12 +135,11 @@ def weighted_sentence_score(sentence_tokens):
     tags = []
     for x in range(len(sentence_tokens)):
         tags.append(sentence_tokens[x][2])
-    return sum([__value_of(tag[0])/len(sentence_tokens) for tag in tags])
-
+    return sum([__value_of(tag[0]) / len(sentence_tokens) for tag in tags])
 
 
 def weighted_sentiment_score(review, matrix_flag):
-    if matrix_flag == True:
+    if matrix_flag:
         return [weighted_sentence_score(sentence) for sentence in review]
     else:
         return sum([weighted_sentence_score(sentence) for sentence in review])
